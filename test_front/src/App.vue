d688017ddd4f5068f2c55cb6f78cefedb4243a86
Rollo <isol@pop-os.localdomain>
2023-09-12 01:47:40 -0600

    Chafita login with vuetify

---
+++ test_front/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+<!--  This the header-->
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          src="https://www.zentric.mx/wp-content/uploads/2023/05/Logo-Zentric.svg"
+          transition="scale-transition"
+          width="100"
+        />
+      </div>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ test_front/src/views/LoginView.vue
@@ -0,0 +1,59 @@
+<template>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Login form</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <form>
+                  <v-text-field
+                      v-model="username"
+                      name="username"
+                      label="Username"
+                      type="text"
+                      placeholder="username"
+                      required
+                  ></v-text-field>
+
+                  <v-text-field
+                      v-model="password"
+                      name="password"
+                      label="Password"
+                      type="password"
+                      placeholder="password"
+                      required
+                  ></v-text-field>
+                  <v-btn type="submit" class="mt-4" color="primary" value="log in"
+                  @click="login">Login</v-btn>
+                </form>
+              </v-card-text>
+            </v-card>
+
+          </v-flex>
+        </v-layout>
+      </v-container>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      const {username} = this;
+      console.log(username + "logged in")
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
