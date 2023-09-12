b4197927217e7499d9c5d025aa99d3ec8f268aa9
aurelio <aurelio.hdz@yandex.com>
2023-09-12 17:48:41 -0600

    #1 Custom User model overriding incremental pk for uuid

--- an_api/UserProfile/models.py
+++ an_api/UserProfile/models.py
@@ -1,3 +1,10 @@
+# std-lib
+import uuid
+# Django
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
