18a86f784ea28977e18d654f74adb636d248623b
aurelio <aurelio.hdz@yandex.com>
2023-09-13 01:47:50 -0600

    #2 Corrected typo

--- an_api/an_api/settings.py
+++ an_api/an_api/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 3rd-party
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
     # Local
     'UserProfile.apps.UserprofileConfig',
 ]
