73b8f56306cca24d7c4ffc19e46a48e40af4fb25
aurelio <aurelio.hdz@yandex.com>
2023-09-13 13:01:25 -0600

    #2 updated urls related to log in and logout

--- an_api/an_api/urls.py
+++ an_api/an_api/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    # Added for log in, log out, and password reset API endpoints
+    path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
 ]
