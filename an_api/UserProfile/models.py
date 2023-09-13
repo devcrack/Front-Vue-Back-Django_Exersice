45b0b013830555f44584ab0e0b8a8b01173ba756
aurelio <aurelio.hdz@yandex.com>
2023-09-12 19:09:22 -0600

    #2 Added some extra properties to Our CustomUser model

--- an_api/UserProfile/models.py
+++ an_api/UserProfile/models.py
@@ -3,8 +3,12 @@ import uuid
 # Django
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomUser(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-
+    # Overriding email field
+    email = models.EmailField(_("email address"), blank=False, unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
