c803738afbe7d3208d16fbab6a30f2a94abf7865
aurelio <aurelio.hdz@yandex.com>
2023-09-13 00:34:08 -0600

    #2 Added CustomUser to django admin

--- an_api/UserProfile/admin.py
+++ an_api/UserProfile/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import CustomUser
 # Register your models here.
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    model = CustomUser
+    search_fields = ['first_name', 'last_name', 'email']
+
+
+admin.site.register(CustomUser, UserProfileAdmin)
+
+
