8a67f19aecd07319696a62e5dd2d50fa23c4072e
aurelio <aurelio.hdz@yandex.com>
2023-09-14 03:34:46 -0600

    #2 Cleaning custum urls

--- an_api/UserProfile/urls.py
+++ an_api/UserProfile/urls.py
@@ -1,13 +1,16 @@
+# Local
 from UserProfile.views import (email_confirm_redirect,
-                                      password_reset_confirm_redirect)
+                               password_reset_confirm_redirect)
+# 3rd-party
 from django.urls import path
 
+
 urlpatterns = [
+
     path('account-confirm-email/<str:key>/',
          email_confirm_redirect, name="account_confirm_email"),
     path(
-        'password/reset/confirm/<str:uidb64>/<str:token>/',password_reset_confirm_redirect,
+        'password/reset/confirm/<str:uidb64>/<str:token>/', password_reset_confirm_redirect,
         name="password_reset_confirm"),
 
 ]
-
