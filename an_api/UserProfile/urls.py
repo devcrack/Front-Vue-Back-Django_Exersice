29f97719f58cc3a841df3617eeb904af5dfe0e46
aurelio <aurelio.hdz@yandex.com>
2023-09-14 14:20:51 -0600

    #2 Added custom and override view to our api paths

--- an_api/UserProfile/urls.py
+++ an_api/UserProfile/urls.py
@@ -1,16 +1,17 @@
 # Local
-from UserProfile.views import (email_confirm_redirect,
+from UserProfile.views import (CustomSignupView,
+                               email_confirm_redirect,
                                password_reset_confirm_redirect)
 # 3rd-party
 from django.urls import path
-
+from dj_rest_auth.registration.views import VerifyEmailView
 
 urlpatterns = [
-
+    path('verify-email/', VerifyEmailView.as_view(), name='rest_verify_email'),
     path('account-confirm-email/<str:key>/',
          email_confirm_redirect, name="account_confirm_email"),
     path(
         'password/reset/confirm/<str:uidb64>/<str:token>/', password_reset_confirm_redirect,
         name="password_reset_confirm"),
-
+    path('', CustomSignupView.as_view())
 ]
