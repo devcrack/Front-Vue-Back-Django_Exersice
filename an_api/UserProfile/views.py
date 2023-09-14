f00669216928b8039e6603e91bf50458bcd5f797
aurelio <aurelio.hdz@yandex.com>
2023-09-14 03:35:25 -0600

    #2 Simple function return just HTTP RESPONSE as plain text

--- an_api/UserProfile/views.py
+++ an_api/UserProfile/views.py
@@ -1,19 +1,21 @@
 # 3rd-party
 from django.shortcuts import render
 from django.conf import settings
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 
 
 # Create your views here.
 
 
-def email_confirm_redirect(request, key):
-    return HttpResponseRedirect(
-        f"{settings.EMAIL_CONFIRM_REDIRECT_BASE_URL}{key}/"
-    )
+def email_confirm_redirect(request, key) -> HttpResponse:
+    # return HttpResponseRedirect(
+    #     f"{settings.EMAIL_CONFIRM_REDIRECT_BASE_URL}{key}/"
+    # )
+    return HttpResponse(f'Redirect to our nice Fronted page\n here is your Key for verified your email: {key}',
+                        content_type='text/plain')
 
 
-def password_reset_confirm_redirect(request, uidb64, token):
+def password_reset_confirm_redirect(request, uidb64, token) -> HttpResponseRedirect:
     return HttpResponseRedirect(
         f"{settings.PASSWORD_RESET_CONFIRM_REDIRECT_BASE_URL}{uidb64}/{token}/"
     )
