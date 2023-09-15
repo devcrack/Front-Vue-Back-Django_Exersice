eb4ba34ed198d9987ccb548b8ec5fa310ec2f8a9
aurelio <aurelio.hdz@yandex.com>
2023-09-15 00:40:08 -0600

    #2 Endpoint for signup lowlevel users

--- an_api/UserProfile/urls.py
+++ an_api/UserProfile/urls.py
@@ -1,5 +1,6 @@
 # Local
 from UserProfile.views import (CustomSignupView,
+                               SignupLowLeveUser,
                                email_confirm_redirect,
                                password_reset_confirm_redirect)
 # 3rd-party
@@ -8,10 +9,9 @@ from dj_rest_auth.registration.views import VerifyEmailView
 
 urlpatterns = [
     path('verify-email/', VerifyEmailView.as_view(), name='rest_verify_email'),
-    path('account-confirm-email/<str:key>/',
-         email_confirm_redirect, name="account_confirm_email"),
-    path(
-        'password/reset/confirm/<str:uidb64>/<str:token>/', password_reset_confirm_redirect,
-        name="password_reset_confirm"),
-    path('', CustomSignupView.as_view(), name='custom_rest_register')
+    path('account-confirm-email/<str:key>/', email_confirm_redirect, name="account_confirm_email"),
+    path('password/reset/confirm/<str:uidb64>/<str:token>/', password_reset_confirm_redirect,
+         name="password_reset_confirm"),
+    path('', CustomSignupView.as_view(), name='custom_rest_register'),
+    path('user/', SignupLowLeveUser.as_view(), name='low_level_rest_register'),
 ]
