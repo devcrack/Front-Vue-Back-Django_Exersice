74c3a1b7e50ff39a28262a71d71e40bb2de1d469
aurelio <aurelio.hdz@yandex.com>
2023-09-15 02:48:01 -0600

    #2 Extending serializers permissions

--- an_api/UserProfile/serializers.py
+++ an_api/UserProfile/serializers.py
@@ -21,7 +21,20 @@ class CustomUserSerializer(RegisterSerializer):
 class LowLevelUserSerializer(CustomUserSerializer):
     group = serializers.CharField(max_length=8, allow_blank=False, required=True)
 
-    def validate_group(self, group):
+    @staticmethod
+    def group_exist(group):
         if not Group.objects.filter(name=group).exists():
             raise serializers.ValidationError("Provided Group Doesn't Exist")
+
+    def validate_group(self, group):
+        self.group_exist(group)
+        return group
+
+
+class LowestLevelUserSerializer(LowLevelUserSerializer):
+
+    def validate_group(self, group):
+        self.group_exist(group)
+        if not group in ['consumer', 'feeder', 'worker']:
+            raise serializers.ValidationError("The Provided Group is Not Valid")
         return group
