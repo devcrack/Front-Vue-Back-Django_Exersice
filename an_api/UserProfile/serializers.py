b544f83f32276bc91393b53b907746caa97d9f83
aurelio <aurelio.hdz@yandex.com>
2023-09-15 00:39:20 -0600

    #2 Added custom serializer for creation user profiles validation

--- an_api/UserProfile/serializers.py
+++ an_api/UserProfile/serializers.py
@@ -3,10 +3,11 @@ from dj_rest_auth.registration.serializers import RegisterSerializer
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 
 
 class CustomUserSerializer(RegisterSerializer):
-    first_name = serializers.CharField(max_length=150, allow_blank=True, required=True)
+    first_name = serializers.CharField(max_length=150, allow_blank=False, required=True)
     last_name = serializers.CharField(max_length=150, allow_blank=False, required=True)
     email = serializers.EmailField(allow_blank=False)
 
@@ -15,3 +16,12 @@ class CustomUserSerializer(RegisterSerializer):
         if user_model.objects.filter(email=email).first():
             raise serializers.ValidationError("Email already exist. Type an unique email")
         return email
+
+
+class LowLevelUserSerializer(CustomUserSerializer):
+    group = serializers.CharField(max_length=8, allow_blank=False, required=True)
+
+    def validate_group(self, group):
+        if not Group.objects.filter(name=group).exists():
+            raise serializers.ValidationError("Provided Group Doesn't Exist")
+        return group
