4ee06ba03326b8537301c208089d499ef3664a79
aurelio <aurelio.hdz@yandex.com>
2023-09-15 00:39:53 -0600

    #2 Added view for signup lowleve users

--- an_api/UserProfile/views.py
+++ an_api/UserProfile/views.py
@@ -1,10 +1,12 @@
 # 3rd-party
+from django.contrib.auth.models import Group
 from django.conf import settings
 from django.http import HttpResponseRedirect, HttpResponse
 from dj_rest_auth.registration.views import RegisterView
-from UserProfile.serializers import CustomUserSerializer
-
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework import status
+# Local
+from UserProfile.serializers import CustomUserSerializer, LowLevelUserSerializer
 
 
 def email_confirm_redirect(request, key) -> HttpResponse:
@@ -23,4 +25,26 @@ def password_reset_confirm_redirect(request, uidb64, token) -> HttpResponseRedir
 
 class CustomSignupView(RegisterView):
     serializer_class = CustomUserSerializer
+
+
+class SignupLowLeveUser(RegisterView):
+    serializer_class = LowLevelUserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        data = self.get_response_data(user)
+
+        if data:
+            response = Response(
+                data,
+                status=status.HTTP_201_CREATED,
+                headers=headers,
+            )
+        else:
+            response = Response(status=status.HTTP_204_NO_CONTENT, headers=headers)
+
+        return response
 
