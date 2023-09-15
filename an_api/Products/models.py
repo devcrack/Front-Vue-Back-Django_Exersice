8a46ca82cb1fa4c57c6473550353b68c2a5bd76b
aurelio <aurelio.hdz@yandex.com>
2023-09-15 15:49:42 -0600

    #3 Products and Branches applications

---
+++ an_api/Products/models.py
@@ -0,0 +1,45 @@
+# std-lib
+import uuid
+# 3rd-party
+from django.db import models
+#Local
+from Branches.models import Branch
+
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50, unique=True, blank=False)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class InventoryRegister(models.Model):
+
+    type_choices = [('entry', 'entry'),
+                    ('issue', 'issue')]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    type = models.CharField(max_length=10, choices=type_choices)
+    register_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.type} ({self.register_date})"
