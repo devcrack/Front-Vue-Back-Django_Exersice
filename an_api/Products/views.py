e28d2c27efe0fc54ff83d25f8d60e2e5d577175e
aurelio <aurelio.hdz@yandex.com>
2023-09-16 14:47:45 -0600

    #3 Base Views of Product App

--- an_api/Products/views.py
+++ an_api/Products/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+# 3rd-party
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+# Local
+from an_api.base_permissions import HasGroupPermission
+from Products.models import (Category,
+                             InventoryRegister,
+                             Product)
+from Products.serializers import (CategoryModelSerializer,
+                                  InventoryModelSerializer,
+                                  ProductModelSerializer)
 
-# Create your views here.
+
+class ProductBaseView(ModelViewSet):
+    permission_classes = [IsAuthenticated, HasGroupPermission]
+    required_groups = {'POST': ['manager', 'worker'],
+                       'GET': ['manager', 'worker', 'consumer']}
+
+
+class CategoryView(ProductBaseView):
+    serializer_class = CategoryModelSerializer
+
+
+class ProductView(ProductBaseView):
+    serializer_class = ProductModelSerializer
+
+
+class InventoryView(ProductBaseView):
+    serializer_class = InventoryModelSerializer
+
+
+
