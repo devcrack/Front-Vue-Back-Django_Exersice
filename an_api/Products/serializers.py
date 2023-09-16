0317c9b7f03c2ac8c29d0cadee263cb49fbefe55
aurelio <aurelio.hdz@yandex.com>
2023-09-16 14:47:00 -0600

    #3 Created Base serializers of Product App

---
+++ an_api/Products/serializers.py
@@ -0,0 +1,24 @@
+# 3rd-party
+from rest_framework.serializers import ModelSerializer
+# Local
+from Products.models import (Category,
+                             InventoryRegister,
+                             Product)
+
+
+class CategoryModelSerializer(ModelSerializer):
+
+    class Meta:
+        model = Category
+
+
+class InventoryModelSerializer(ModelSerializer):
+
+    class Meta:
+        model = InventoryRegister
+
+
+class ProductModelSerializer(ModelSerializer):
+
+    class Meta:
+        model = Product
