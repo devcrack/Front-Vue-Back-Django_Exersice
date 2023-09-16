41c775a2b16c4ae354e2805fcfe642027eed64fe
aurelio <aurelio.hdz@yandex.com>
2023-09-16 12:19:49 -0600

    #3 Test were adjusted to final changes in ProfileUser

--- an_api/UserProfile/tests/test_user_profile.py
+++ an_api/UserProfile/tests/test_user_profile.py
@@ -3,138 +3,83 @@ import json
 
 # 3rd-party
 import pytest
+from django.contrib.auth.models import Group
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 from rest_framework.authtoken.models import Token
 
-from boilerplate import create_user, create_token
-
-
-registration_url = reverse('custom_rest_register')
-
+from boilerplate import (create_super_user,
+                         create_token,
+                         create_user,
+                         get_authenticated_client)
 
 pytestmark = pytest.mark.django_db
 
 
-def test_registration(client) -> None:
-    registration_data = {
-        'username': 'test@email.com',
-        'password1': 'awesome_password',
-        'password2': 'awesome_password',
-        'email': 'test@email.com',
-        "first_name": "Carlos",
-        "last_name": "Pensantes"
+def test_register_manager_user_should_succed() -> None:
+    admin_user = create_super_user()
+
+    token = create_token(admin_user)
+
+    client = get_authenticated_client(token)
+
+    payload = {
+        'username': 'andrew',
+        'email': 'andrea_del_arco@mail.com',
+        'password1': 'mientras123',
+        'password2': 'mientras123',
+        'first_name': 'Andreita',
+        'last_name': 'Bonita',
+        'group': 'manager'
     }
 
-    response = client.post(path=registration_url, data=registration_data)
+    response = client.post(path=reverse('low_level_rest_register'),
+                           data=payload)
 
     assert response.status_code == status.HTTP_201_CREATED
-    data = response.json()
-
-    assert 'key' in data
 
 
-def test_registration_should_fail_password_to_short(client) -> None:
-    registration_data = {
-        'username': 'test@email.com',
-        'password1': 'test',
-        'password2': 'test',
-        'email': 'test@email.com',
-        "first_name": "Carlos",
-        "last_name": "Pensantes"
+def test_create_lowest_level_users_should_pass() -> None:
+    user = create_user()
+    user.groups.add(Group.objects.get(name='manager'))
+
+    token = create_token(user)
+
+    client = get_authenticated_client(token)
+
+    payload = {
+        "username": "anton",
+        "email": "anton_del_arco@mail.com",
+        "password1": "mientras123",
+        "password2": "mientras123",
+        "first_name": "Antonello",
+        "last_name": "Feo",
+        "group": "worker"
     }
+    response = client.post(path=reverse('lowest_level_rest_register'), data=payload)
 
-    response = client.post(path=registration_url, data=registration_data)
-
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
-
-    body_error = {
-        "password1": [
-            "This password is too short. It must contain at least 8 characters.",
-            "This password is too common."
-        ]
-    }
-
-    assert json.loads(response.content) == body_error
+    assert response.status_code == status.HTTP_201_CREATED
 
 
-def test_registration_should_fail_email_duplication(client) -> None:
-    create_user(**{'username': 'a_username2',
-                   'email': 'awesome_mail1@mail.com',
-                   'password': 'mientras123',
-                   'first_name': 'Carlos',
-                   'last_name': 'Pensantes'
-                   })
-
-    registration_data = {'username': 'a_username2',
-                         'email': 'awesome_mail1@mail.com',
-                         'password1': 'mientras123',
-                         'password2': 'mientras123',
-                         'first_name': 'test_name',
-                         'last_name': 'test_last_name'
-                         }
-
-    response = client.post(path=registration_url, data=registration_data)
-
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
-
-
-def test_registration_should_fail_username_duplication(client) -> None:
-    create_user(**{'username': 'a_username3',
-                   'email': 'a_new_awesome_mail@mail.com',
-                   'password': 'mientras123',
-                   'first_name': 'test_name',
-                   'last_name': 'test_last_name'
-                   })
-
-    registration_data = {'username': 'a_username3',
-                         'email': 'awesome_mail@mail.com',
-                         'password1': 'mientras123',
-                         'password2': 'mientras123',
-                         'first_name': 'test_name',
-                         'last_name': 'test_last_name'
-                         }
-
-    response = client.post(path=registration_url, data=registration_data)
-
-    body_error = {
-        "username": [
-            "A user with that username already exists."
-        ]
-    }
-
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert json.loads(response.content) == body_error
 
 
 def test_login_success(client) -> None:
 
-    create_user(**{'username': 'test_username',
-                   'email': 'test@example.com',
-                   'password': 'testpassword',
-                   'first_name': 'test_name',
-                   'last_name': 'test_last_name'
-                   })
+    create_user()
 
     response = client.post(path=reverse('rest_login'),
-                           data={'email': 'test@example.com',
-                                 'password': 'testpassword'})
+                           data={'email': 'a_user@mail.com',
+                                 'password': 'mientras123'})
 
     assert response.status_code == status.HTTP_200_OK
 
 
 def test_logout_success() -> None:
-    user = create_user(**{'username': 'test_username',
-                          'email': 'test@example.com',
-                          'password': 'testpassword',
-                          'first_name': 'test_name',
-                          'last_name': 'test_last_name'
-                          })
+    user = create_user()
     token = create_token(user)
-    client = APIClient()
 
-    client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+    client = get_authenticated_client(token)
 
     response = client.post(reverse('rest_logout'))
 
@@ -146,23 +91,11 @@ def test_logout_success() -> None:
 
 
 def test_get_user_detail_should_pass() -> None:
-    client = APIClient()
+    user = create_user()
+    token = create_token(user)
 
+    client = get_authenticated_client(token)
 
-    registration_data = {
-        'username': 'test@email.com',
-        'password1': 'awesome_password',
-        'password2': 'awesome_password',
-        'email': 'test@email.com',
-        "first_name": "Carlos",
-        "last_name": "Pensantes"
-    }
-
-    response = client.post(path=registration_url, data=registration_data)
-    print(json.loads(response.content))
-    assert response.status_code == status.HTTP_201_CREATED
-    data = response.json()
-    client.credentials(HTTP_AUTHORIZATION='Token ' + data.get('key'))
     response = client.get(reverse('rest_user_details'))
 
     assert response.status_code == status.HTTP_200_OK
