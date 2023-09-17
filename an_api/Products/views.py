6705e87def1be0a38c6e07a910d72f2a592bc469
aurelio <aurelio.hdz@yandex.com>
2023-09-17 03:20:30 -0600

    #3 Added permissions and deleted correctly handled

--- an_api/Products/views.py
+++ an_api/Products/views.py
@@ -1,6 +1,7 @@
 # 3rd-party
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 # Local
 from an_api.base_permissions import HasGroupPermission
 from Products.models import (Category,
@@ -14,7 +15,22 @@ from Products.serializers import (CategoryModelSerializer,
 class ProductBaseView(ModelViewSet):
     permission_classes = [IsAuthenticated, HasGroupPermission]
     required_groups = {'POST': ['manager', 'worker'],
+                       'PUT': ['manager', 'worker'],
+                       'DELETE': ['manager'],
                        'GET': ['manager', 'worker', 'consumer']}
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        previous_stock = instance.stock
+
+        instance.deleted = True
+
+        if previous_stock > 0:
+            instance.stock = 0
+            InventoryRegister.objects.create(product=instance, quantity=0, type='issue')
+        instance.save()
+
+        return Response({'message': 'Product was successfully deleted'})
 
 
 class CategoryView(ProductBaseView):
