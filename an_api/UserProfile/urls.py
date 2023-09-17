218be4fd06947262a137a67e254996fe93e8f95c
aurelio <aurelio.hdz@yandex.com>
2023-09-17 00:30:31 -0600

    #3 Modificacions registration user endpoints

--- an_api/UserProfile/urls.py
+++ an_api/UserProfile/urls.py
@@ -1,18 +1,24 @@
+# 3rd-party
+from django.urls import path, include
+from dj_rest_auth.registration.views import VerifyEmailView
+from rest_framework.routers import SimpleRouter
 # Local
 from UserProfile.views import (SignupLowLeveUserView,
+                               CustomUserListView,
                                email_confirm_redirect,
                                password_reset_confirm_redirect)
-# 3rd-party
-from django.urls import path
-from dj_rest_auth.registration.views import VerifyEmailView
+
+router = SimpleRouter()
+
+router.register('users-list', CustomUserListView, basename='user-detail')
 
 urlpatterns = [
-    path('verify-email/', VerifyEmailView.as_view(), name='rest_verify_email'),
-    path('account-confirm-email/<str:key>/', email_confirm_redirect, name="account_confirm_email"),
-    path('password/reset/confirm/<str:uidb64>/<str:token>/', password_reset_confirm_redirect,
+    path('registration/verify-email/', VerifyEmailView.as_view(), name='rest_verify_email'),
+    path('registration/account-confirm-email/<str:key>/', email_confirm_redirect, name="account_confirm_email"),
+    path('registration/password/reset/confirm/<str:uidb64>/<str:token>/', password_reset_confirm_redirect,
          name="password_reset_confirm"),
     # Not exposed for security Issues
     # path('', CustomSignupView.as_view(), name='custom_rest_register'),
-    path('user/', SignupLowLeveUserView.as_view(), name='low_level_rest_register'),
-    # path('org-user/', SignupLowestLeveUsers.as_view(), name='lowest_level_rest_register')
+    path('registration/user/', SignupLowLeveUserView.as_view(), name='low_level_rest_register'),
+    path('', include(router.urls)),
 ]
