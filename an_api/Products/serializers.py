af25b9e48c845a088b1beb9ba244290b828e5b65
aurelio <aurelio.hdz@yandex.com>
2023-09-17 03:07:31 -0600

    #3 product updating  well handled

--- an_api/Products/serializers.py
+++ an_api/Products/serializers.py
@@ -29,7 +29,34 @@ class ProductModelSerializer(ModelSerializer):
         stock = validated_data.get('stock', 0)
         instance = super().create(validated_data)
         if stock:
-            InventoryRegister.objects.create(product=instance, quantity=stock, tipo='entry')
+            InventoryRegister.objects.create(product=instance, quantity=stock, type='entry')
 
         return instance
+
+    def update(self, instance, validated_data):
+        previous_stock = instance.stock
+        new_stock = validated_data.get('stock', instance.stock)
+
+        if previous_stock != new_stock:
+            move_type = 'entry' if new_stock > previous_stock else 'issue'
+            _quantity = abs(new_stock - previous_stock)
+            InventoryRegister.objects.create(product=instance, quantity=_quantity, type=move_type)
+
+        instance.name = validated_data.get('name', instance.name)
+        instance.price = validated_data.get('price', instance.price)
+        instance.category = validated_data.get('category', instance.category)
+        instance.stock = new_stock
+
+        instance.save()
+
+        return instance
+
+
+
+
+
+
+
+
+
 
