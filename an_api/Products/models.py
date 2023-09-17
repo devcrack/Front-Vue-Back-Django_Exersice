391fb755d9a4a82de10c0bf3465a41f78d8e9189
aurelio <aurelio.hdz@yandex.com>
2023-09-17 02:20:50 -0600

    #3 If stock is not provided default is 0

--- an_api/Products/models.py
+++ an_api/Products/models.py
@@ -12,7 +12,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.IntegerField()
+    stock = models.IntegerField(default=0)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     creation_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
