2b1f5dff7b7a11aa402089f49932191a49452392
aurelio <aurelio.hdz@yandex.com>
2023-09-17 00:30:02 -0600

    #3 some modifications for registration and list users endpoints

--- an_api/an_api/urls.py
+++ an_api/an_api/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('an_api/auth/', include('dj_rest_auth.urls')),
-    path('an_api/registration/', include('UserProfile.urls')),
+    path('an_api/', include('UserProfile.urls')),
     path('an_api/', include('Products.urls')),
 ]
