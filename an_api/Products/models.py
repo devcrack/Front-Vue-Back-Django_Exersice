ed314bdfd7b8eac46df6c8c1f40ff6be73f80e5d
aurelio <aurelio.hdz@yandex.com>
2023-09-17 20:10:07 -0600

    #3 Added related name for search inventory records from product instance

--- an_api/Products/models.py
+++ an_api/Products/models.py
@@ -38,7 +38,7 @@ class InventoryRegister(models.Model):
                     ('deleted', 'deleted')]
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='InventoryRegister')
     quantity = models.IntegerField()
     type = models.CharField(max_length=10, choices=type_choices)
     register_date = models.DateTimeField(auto_now_add=True)
--- an_api/Products/urls.py
+++ an_api/Products/urls.py
@@ -1,15 +1,17 @@
 # 3rd-party
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 # Local
 from Products.views import CategoryView, InventoryView, ProductView
 
-
 router = SimpleRouter()
 
 router.register(r'categories', CategoryView, basename='category')
-router.register(f'inventory-registers', InventoryView, basename='inventory-register')
 router.register(f'products', ProductView, basename='product')
 
+urlpatterns = [
+    path(f'inventory-register/<uuid:product_pk>/', InventoryView.as_view({'get': 'list'}), name='inventory_register'),
+    path('', include(router.urls)),
+]
 
-urlpatterns = router.urls
+
